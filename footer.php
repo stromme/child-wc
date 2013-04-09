a5e5cd37b6e8f7f05f88bab9c9d48c9bb1195f59
Lars <lars@stromme.ca>
2013-04-09 18:14:22 +0545

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * .container div element.
+ *
+ * @package Hatch
+ * @subpackage 
+ * @since 
+ */
+?>
+	<section class="footer">
+		<img src="<?php echo THEME_IMAGES; ?>brand/window-cleaning-dot-com-logo.png">
+		<h3 class="bumper-bottom bumper-top">Like to chat? Call our team anytime at (123) 123-1234 or <a href="">get an online estimate.</a></h3>
+		<p class="footer-links"><a href="">Visit WindowCleaning.com</a><a href="">Our Locations</a><a href="">News About Us</a><a href="">Apply for Membership</a></p>
+		<p><small>North America's Best Window Cleaners | Copyright 2013 | <a href="">Privacy</a> | <a href="">Terms</a></small></p>
+	</section>
+
+</div><!-- / .container -->
+<? get_template_part('templates/modal', 'review'); ?>
+<? get_template_part('templates/modal', 'lead'); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
