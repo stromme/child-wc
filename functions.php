c8f460ce4495b9e447a98a5816c80a80de2d5442
Lars <lars@stromme.ca>
2013-04-18 10:47:34 +0545

    Add google publisher link

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php
 
-function custom_login_logo() {
+function hs_custom_login_logo() {
 	echo '<style type="text/css">
 		.login h1 a, #login h1 a { background-image: url('.get_stylesheet_directory_uri().'/images/logo.png) !important; background-size: 285px 155px; padding-bottom: 100px; }
 	</style>';
 }
-add_action('login_head', 'custom_login_logo');
+add_action('login_head', 'hs_custom_login_logo');
+
+function hs_google_publisher() {
+	echo '<link href="https://plus.google.com/110109148861996385232" rel="publisher" />';
+}
+add_action('wp_head', 'hs_google_publisher');
 
 ?>
