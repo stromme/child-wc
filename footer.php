0f1b777a6c5436194365a62028dbb75c84d5becd
Lars <lars@stromme.ca>
2013-04-21 12:58:07 +0545

    Fix locations link

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 	<section class="footer bg-slate-invert" itemprop="member" itemscope="http://schema.org/Organization">
 		<img src="<?php echo THEME_IMAGES; ?>brand/window-cleaning-dot-com-logo.png" itemprop="logo">
 		<h3 class="bumper-bottom bumper-top">Like to chat? Call our team anytime at <?=get_phone_number()?> or <a href="" class="quick-estimate">get an online estimate.</a></h3>
-		<p class="footer-links"><a href="http://www.windowcleaning.com" rel="nofollow">Visit <span itemprop="name">WindowCleaning.com</span></a><a href="http://www.windowcleaning.com/locations" rel="nofollow">Our Locations</a><a href="http://www.windowcleaning.com/news" rel="nofollow">News About Us</a><a href="http://www.windowcleaning.com/members">Apply for Membership</a></p>
-		<p><small><span itemprop="description">North America's Best Window Cleaners</span> | Copyright 2013 | <a href="">Privacy</a> | <a href="">Terms</a></small></p>
+		<p class="footer-links"><a href="<?=get_site_url(1)?>" rel="nofollow">Visit <span itemprop="name">WindowCleaning.com</span></a><a href="<?=get_site_url(1)?>/locations" rel="nofollow">Our Locations</a><a href="<?=get_site_url(1)?>/news" rel="nofollow">News About Us</a><a href="<?=get_site_url(1)?>/members">Apply for Membership</a></p>
+		<p><small><span itemprop="description">North America's Best Window Cleaners</span> | Copyright 2013 | <a href="<?=get_site_url(1)?>/privacy">Privacy</a> | <a href="<?=get_site_url(1)?>/terms">Terms</a></small></p>
 	</section>
 
 </div><!-- / .container -->
