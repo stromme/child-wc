1712a8fb41973d0db69b11344f6743659ee59a69
Josua Leonard <josua.leonard@gmail.com>
2014-04-25 13:41:08 +0700

    Update year

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<img src="<?php echo THEME_IMAGES; ?>brand/window-cleaning-dot-com-logo.png" itemprop="logo">
 		<h3 class="bumper-bottom bumper-top">Like to chat? Call our team anytime at <?=get_phone_number()?> or <a href="javascript:void(0);" class="quick-estimate <?=apply_filters('apply_responsibid', false);?>">get an online estimate.</a></h3>
 		<p class="footer-links"><a href="<?=get_site_url(1)?>" rel="nofollow" itemprop="name">WindowCleaning.com</a><a href="<?=get_site_url(1)?>/locations" rel="nofollow">Our Locations</a><a href="<?=get_site_url(1)?>/hub/" rel="nofollow">News About Us</a><a href="<?=get_site_url(1)?>/members">Apply for Membership</a></p>
-		<p><small><span itemprop="description">North America's Best Window Cleaners</span> | Copyright 2013 | <a href="<?=get_site_url(1)?>/privacy">Privacy</a> | <a href="<?=get_site_url(1)?>/terms">Terms</a></small></p>
+		<p><small><span itemprop="description">North America's Best Window Cleaners</span> | Copyright <?=date('Y')?> | <a href="<?=get_site_url(1)?>/privacy">Privacy</a> | <a href="<?=get_site_url(1)?>/terms">Terms</a></small></p>
     <?php
       $social_media = TB_Frontend::get_social_media_link();
       if($social_media){
